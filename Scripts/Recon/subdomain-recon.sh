8b42a88aea9bdfb42c7e2bac644309248e72e7f2
Breno Amaral <26623301+BrAmaral@users.noreply.github.com>
2023-02-02 20:49:50 -0300

    Update subdomain-recon.sh

--- Scripts/Recon/subdomain-recon.sh
+++ Scripts/Recon/subdomain-recon.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
-# You need to have assetfinder, subfinder & amass installed
-
 url=$1
 
 if [ ! -d "$url" ];then
 	mkdir $url
-fi 
+fi
 
-echo "[+] Running assetfinder..."
+echo "[+] Executing assetfinder..."
 assetfinder $url >> $url/tmp.txt
 
-echo "[+] Running subfinder..."
+echo "[+] Executing subfinder..."
 subfinder -d $url >> $url/tmp.txt
 
-echo "[+] Running Amass..."
-amass enum -d $url -o $url/amass_tmp.txt
-cat $url/amass_tmp >> $url/tmp.txt
-rm -rf $url/amass_tmp.txt
+echo "[+] Executing amass..."
+amass enum -d $url -o $url/tmp_amass.txt
+cat $url/tmp_amass.txt >> $url/tmp.txt
+rm -rf $url/tmp_amass.txt
 
 echo "[+] Compiling results..."
-cat $url/tmp.txt | sort --unique >> $url/recon-results.txt
+cat $url/tmp.txt | sort --unique > $url/recon-result.txt
 rm -rf $url/tmp.txt
 
-echo "[=] Results are compiled on the file recon-results.txt!"
+echo "[+] Probing websites..."
+cat $url/recon-result.txt | httprobe -prefer-https | sed 's/^http\(\|s\):\/\///g' >> $url/probed-result.txt
 
+echo "[+] Finding vulnerabilities with nuclei..."
+nuclei -ut
+nuclei -l $url/probed-result.txt -o $url/nuclei-result.txt -json
